313e40a7f2b9d500027eb6aa889e129c7fac65ff
marketvibor.ru <oleg@lemon-art.ru>
2019-03-13 11:47:24 +0300

    request partners

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -11,7 +11,7 @@
                 'items' => [
                     ['label' => 'Заявки', 'icon' => 'file-code-o', 'url' => ['/orders']],
 					['label' => 'Заказ звонка', 'icon' => 'phone', 'url' => ['/request']],
-					['label' => 'Заказа сотрудничества', 'icon' => 'handshake-o', 'url' => ['/request']],
+					['label' => 'Заказ сотрудничества', 'icon' => 'handshake-o', 'url' => ['/requestp']],
                     ['label' => 'Контент', 'icon' => 'newspaper-o', 'url' => ['/debug'],
 						'items' => [
 							['label' => 'Предложения', 'icon' => 'circle-o', 'url' => ['/offers'],],
