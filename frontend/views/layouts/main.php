ae5d4562182011d4d4d488674b53819800750714
Sergey <wlendys@yandex.ru>
2019-06-25 15:04:04 +0300

    !

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -513,7 +513,7 @@ _tmr.push({id: "3083494", type: "pageView", start: (new Date()).getTime()});
 	//шорт тег в title
 	$(document).ready(function(){
 		var content = $('title').html();	
-		document.title = content.replace(/{city_dec4}/gi, "<?php if ($city['dec1'] != 'в России') { echo $city['dec4']; } ?>");
+		//document.title = content.replace(/{city_dec4}/gi, "<?php //if ($city['dec1'] != 'в России') { echo $city['dec4']; } ?>");
 		document.title = content.replace(/{city_dec3}/gi, "<?php if ($city['dec1'] != 'в России') { echo $city['dec3']; } ?>");
 		document.title = content.replace(/{city_dec2}/gi, "<?php if ($city['dec1'] != 'в России') { echo $city['dec2']; } ?>");
 		document.title = content.replace(/{city_dec1}/gi, "<?php if ($city['dec1'] != 'в России') { echo $city['dec1']; } ?>");
