824cfca59b4f6c22b864e3126d416a0520fd8da9
Sergey <wlendys@yandex.ru>
2019-06-28 23:43:01 +0300

    !

--- frontend/web/robots.php
+++ frontend/web/robots.php
@@ -6,38 +6,32 @@ if ($subdomain == 'dev' || $subdomain == 'marketvibor') {
 } else {
     $subdomain = $subdomain.'.';
 } ?>
+<?header("Content-Type:text/plain");?>
 
-<html>
-    <head></head>
-    <body>
-        <pre style="word-wrap: break-word; white-space: pre-wrap;">
-            User-agent: *
-            Disallow: /backend/
-            Disallow: /common/
-            Disallow: /*index.php$
-            Disallow: /user/
-            Disallow: /admin/
-            Disallow: /*?q=
-            Disallow: /*personal
-            Sitemap: https://<?= $subdomain ?>marketvibor.ru/sitemap.xml
+User-agent: *
+Disallow: /backend/
+Disallow: /common/
+Disallow: /*index.php$
+Disallow: /user/
+Disallow: /admin/
+Disallow: /*?q=
+Disallow: /*personal
+Sitemap: https://<?= $subdomain ?>marketvibor.ru/sitemap.xml
 
-            #User-agent: Google
-            #Sitemap: https://<?= $subdomain ?>marketvibor.ru/video-sitemap.xml
+#User-agent: Google
+#Sitemap: https://<?= $subdomain ?>marketvibor.ru/video-sitemap.xml
 
-            User-agent: Yandex
-            Disallow: /backend/
-            Disallow: /common/
-            Disallow: /*index.php$
-            Disallow: /user/
-            Disallow: /admin/
-            Disallow: /*?q=
-            Disallow: /*personal
-            Host: https://<?= $subdomain ?>MarketVibor.ru
-            Sitemap: htps://<?= $subdomain ?>marketvibor.ru/sitemap.xml
+User-agent: Yandex
+Disallow: /backend/
+Disallow: /common/
+Disallow: /*index.php$
+Disallow: /user/
+Disallow: /admin/
+Disallow: /*?q=
+Disallow: /*personal
+Host: https://<?= $subdomain ?>MarketVibor.ru
+Sitemap: htps://<?= $subdomain ?>marketvibor.ru/sitemap.xml
 
-            User-agent: AhrefsBot
-            Disallow: /
-        </pre>
-    </body>
-</html>
+User-agent: AhrefsBot
+Disallow: /
 
