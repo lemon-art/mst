5f8764377df83e38c5a88e08ac06ea950aec1c05
Sergey <wlendys@yandex.ru>
2019-07-01 14:56:11 +0300

    !

--- frontend/web/sitemap.php
+++ frontend/web/sitemap.php
@@ -61,7 +61,8 @@ if ($subdomain == 'dev' || $subdomain == 'marketvibor') {
             }
         </style>
     </head>
-    <body><div id="webkit-xml-viewer-source-xml">
+    <body>
+        <div id="webkit-xml-viewer-source-xml">
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
             <url>
                 <loc>https://<?= $subdomain ?>marketvibor.ru/</loc>
@@ -309,5 +310,6 @@ if ($subdomain == 'dev' || $subdomain == 'marketvibor') {
                 <priority>0.8</priority>
             </url>
         </urlset>
+        </div>
     </body>
 </html>
