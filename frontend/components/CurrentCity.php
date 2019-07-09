d5136400b4bd29300446c3015ed0da802a8b6506
Sergey <wlendys@yandex.ru>
2019-07-09 12:03:16 +0300

    *

--- frontend/components/CurrentCity.php
+++ frontend/components/CurrentCity.php
@@ -20,7 +20,7 @@ class CurrentCity extends Model
         $city = array('name' => '', 'dec1' => '', 'dec2' => '', 'dec3' => '', 'dec4' => '');
         $rus = 'в России';
         if ($subdomain == 'dev' || $subdomain == 'marketvibor') {
-            $city['dec1'] = $rus;
+            $city = array('name' => 'Россия', 'dec1' => 'в России', 'dec2' => 'в Россию', 'dec3' => 'Российский', 'dec4' => 'России');
         } else {
             $city = City::find()->where(['subdomain' => $subdomain])->one();
             $city = (array)$city;
