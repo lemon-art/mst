78d6f965c82cb30579bed84bdf5cf2a39c8c8ec1
Sergey <wlendys@yandex.ru>
2019-07-15 10:45:06 +0300

    *

--- frontend/views/layouts/sitemap.php
+++ frontend/views/layouts/sitemap.php
@@ -1,1 +1,25 @@
-<?php // ?>
+<?php
+
+use yii\helpers\Html;
+use frontend\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <?= Html::csrfMetaTags() ?>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?= $content ?>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
