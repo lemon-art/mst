d39c249c6cd76676482beaa13bc67787b1ff706d
Sergey <wlendys@yandex.ru>
2019-07-30 20:20:47 +0300

    *

--- backend/views/credit-filter/create.php
+++ backend/views/credit-filter/create.php
@@ -9,6 +9,8 @@ use yii\helpers\Html;
 $this->title = 'Добавить ссылку и фильтр кредитов';
 $this->params['breadcrumbs'][] = ['label' => 'Фильтры кредитов', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+$model->category = 0; //популярное по умолчанию
 ?>
 <div class="credit-filter-create">
 
