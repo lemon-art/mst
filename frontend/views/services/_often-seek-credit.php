ed0d8f9d9f6a4dd7b78955e6f89b9f25dc0002ec
Sergey <wlendys@yandex.ru>
2019-07-30 19:45:59 +0300

    *

--- frontend/views/services/_often-seek-credit.php
+++ frontend/views/services/_often-seek-credit.php
@@ -6,41 +6,44 @@ use backend\models\CreditFilter;
 
 
 ?>
-<div class="container often-seek-credit">
-    <div class="row">
-        <h2>Часто ищут</h2>
+<div class="often-seek-credit">
+    <div class="container">
+        <h2><b>Часто ищут</b></h2>
+        <div class="row">
 
-        <div class="col-sm-6 col-md-6 col-lg-3">
-            <h4><b>Популярное</b></h4>
-            <?php foreach ($credit_filter as $arr){
-                if ($arr['category'] = 0) {
-                    echo $arr['url_name'];
-                }
-            } ?>
-        </div>
-        <div class="col-sm-6 col-md-6 col-lg-3">
-            <h4><b>Рекомендованное</b></h4>
-            <?php foreach ($credit_filter as $arr){
-                if ($arr['category'] = 1) {
-                    echo $arr['url_name'];
-                }
-            } ?>
-        </div>
-        <div class="col-sm-6 col-md-6 col-lg-3">
-            <h4><b>Популярные суммы</b></h4>
-            <?php foreach ($credit_filter as $arr){
-                if ($arr['category'] = 2) {
-                    echo $arr['url_name'];
-                }
-            } ?>
-        </div>
-        <div class="col-sm-6 col-md-6 col-lg-3">
-            <h4><b>По сроку</b></h4>
-            <?php foreach ($credit_filter as $arr){
-                if ($arr['category'] = 3) {
-                    echo $arr['url_name'];
-                }
-            } ?>
+
+            <div class="col-sm-6 col-md-6 col-lg-3">
+                <h4><b>Популярное</b></h4>
+                <?php foreach ($credit_filter as $arr){
+                    if ($arr['category'] == 0) {
+                        echo $arr['url_name'];
+                    }
+                } ?>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+                <h4><b>Рекомендованное</b></h4>
+                <?php foreach ($credit_filter as $arr){
+                    if ($arr['category'] == 1) {
+                        echo $arr['url_name'];
+                    }
+                } ?>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+                <h4><b>Популярные суммы</b></h4>
+                <?php foreach ($credit_filter as $arr){
+                    if ($arr['category'] == 2) {
+                        echo $arr['url_name'];
+                    }
+                } ?>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-3">
+                <h4><b>По сроку</b></h4>
+                <?php foreach ($credit_filter as $arr){
+                    if ($arr['category'] == 3) {
+                        echo $arr['url_name'];
+                    }
+                } ?>
+            </div>
         </div>
     </div>
 </div>
