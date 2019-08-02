539dc6c5a10ac413d20c5fb2a8731dc6f826564b
Sergey <wlendys@yandex.ru>
2019-08-02 15:38:07 +0300

    *

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -228,7 +228,8 @@ $city = CurrentCity::currentCity();
    ?>
     <?if ( Yii::$app->request->url !== Yii::$app->homeUrl ):?>
 	
-		<?if ( strpos(Yii::$app->request->url, 'services')  === false || strpos(Yii::$app->request->url, 'credit')  === false ):?>
+		<?php//if ( (strpos(Yii::$app->request->url, 'services')  === false) || (strpos(Yii::$app->request->url, 'credit')  === false) ):?>
+		<?if ( Yii::$app->controller->id  != 'services' || Yii::$app->controller->id  != 'credit' ):?>
 			<section class="section_inner">
 				<div class="cont">
 					<?=
