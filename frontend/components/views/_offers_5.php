5a818b56c95eb1aef564babf41e61e48b047001e
Sergey <wlendys@yandex.ru>
2019-09-02 13:02:31 +0300

    *

--- frontend/components/views/_offers_5.php
+++ frontend/components/views/_offers_5.php
@@ -21,11 +21,11 @@ $model['rate'] /= 100;
 										<div class="number"><?=$index?></div>
 
 
-
+										<?php if ($model->image) { ?>
+											<img width="100%" height="100px" src="<?=Files::getPath($model->image)?>" alt="<?=$model['name']?>">
+										<?php } ?>
 										<div class="ic_bank">
-											<?php if ($model->image) { ?>
-												<img width="100%" height="100px" src="<?=Files::getPath($model->image)?>" alt="<?=$model['name']?>">
-											<?php } else if ($model->banks->image) { ?>
+											<?php if ($model->banks->image) { ?>
 												<img src="<?=Files::getPath($model->banks->image)?>" alt="<?=$model['name']?>">
 											<?php } ?>
 
